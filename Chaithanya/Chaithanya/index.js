d094911c6760f580a21fc8c4731ba94bf9a5c642
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-06-03 16:55:16 +0530

    Add files via upload

---
+++ Chaithanya/Chaithanya/index.js
@@ -0,0 +1,18 @@
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("Navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+window.onload = function () {
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "chaitu786", { responsive: true });
+}
